fb6eba8f10e345eee857019dfb69cf791a55d1fe
Diego Gonzalez <diego.gonzalez@one-click.com.co>
2017-06-04 22:30:34 -0500

    ldap

--- views/cadena-aprobacion/_search.php
+++ views/cadena-aprobacion/_search.php
@@ -15,7 +15,7 @@ use yii\widgets\ActiveForm;
         'method' => 'get',
     ]); ?>
         <?= $form->field($model, 'nombre')->textInput(['placeholder' => "nombre"])->label(false) ?>
-        <?= $form->field($model, 'estado')->dropDownList([1=>'Activo',0=>'No Activo'],['prompt'=>'Seleccione un rol'])->label(false) ?>
+        <?= $form->field($model, 'estado')->dropDownList([1=>'Activo',0=>'No Activo'],['prompt'=>'Seleccione un estado'])->label(false) ?>
         <div class="form-group">
             <?= Html::submitButton('<i class="glyphicon glyphicon-search"></i> Filtrar', ['class' => 'btn btn-info']) ?>
             <?= Html::resetButton('<i class="fa fa-paint-brush" aria-hidden="true"></i> limpiar Filtros', ['class' => 'btn btn-danger', 'onclick'=>' limiparFiltros( this ) ' ]) ?>
