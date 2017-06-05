2c2b27b3d58bb232b2ca1a2cd5dfdc4e310e58db
Diego Gonzalez <diego.gonzalez@one-click.com.co>
2017-06-04 21:37:37 -0500

    ldap

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -89,7 +89,7 @@ class LoginForm extends Model
                 echo "<br\>";
                 $usuario->nombre = $usuarioLdap[ Yii::$app->params['nombre'] ][0];
                 echo "//////////-*";
-                print_r($usuarioLdap[ Yii::$app->params['nombre'] ]);
+                print_r($usuarioLdap->mail);
                 echo "*-/////////////";
                 $usuario->apellido = $usuarioLdap[ Yii::$app->params['apellido'] ][0];
                 $usuario->email = $usuarioLdap[ Yii::$app->params['email'] ][0];
