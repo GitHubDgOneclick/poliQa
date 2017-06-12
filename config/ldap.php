7e7680c2306ed62fe84c97f3ddc48d53973a8e97
Diego Gonzalez <diego.gonzalez@one-click.com.co>
2017-06-12 13:32:31 -0500

    commit espacio de trabajo maquina virtual

--- config/ldap.php
+++ config/ldap.php
@@ -4,11 +4,11 @@ return [
         'class'=>'Edvlerblog\Ldap',
         'options'=> [
                 'ad_port'      => 389,
-                'domain_controllers'    => array( '172.31.5.195', '172.31.30.159' ),
+                'domain_controllers'    => array( '127.0.0.1', '127.0.0.1' ),
                 'account_suffix' =>  '@poli.edu.co',
-                'base_dn' => "DC=poli,DC=edu,DC=co",
+                'base_dn' => "dc=poli,dc=edu,dc=co",
                 // for basic functionality this could be a standard, non privileged domain user (required)
-                'admin_username' => 'admin',
+                'admin_username' => 'cn=admin,dc=poli,dc=edu,dc=co',
                 'admin_password' => 'ldapQapassw0rd'
             ],
             //Connect on Adldap instance creation (default). If you don't want to set password via main.php you can
